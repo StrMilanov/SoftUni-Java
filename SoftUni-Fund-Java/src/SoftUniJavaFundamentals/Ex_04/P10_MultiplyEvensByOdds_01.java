4924098ca083c4c6a76a052a808e3bd50d78b314
Stratsimir Milanov <114015950+StrMilanov@users.noreply.github.com>
2022-10-07 10:41:50 +0300

    Add files via upload

---
+++ SoftUni-Fund-Java/src/SoftUniJavaFundamentals/Ex_04/P10_MultiplyEvensByOdds_01.java
@@ -0,0 +1,42 @@
+package SoftUniJavaFundamentals.Ex_04;
+
+import java.util.Scanner;
+
+public class P10_MultiplyEvensByOdds_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numInput = Math.abs(Integer.parseInt(scanner.nextLine()));
+
+        System.out.println(multiplySumOfEvenAndOdd(numInput));
+    }
+
+    public static int multiplySumOfEvenAndOdd(int num) {
+        return sumEvenNumbers(num) * sumOddNumbers(num);
+    }
+
+    public static int sumEvenNumbers(int n) {
+        int sumEvenNumbers = 0;
+        while (n > 0) {
+            int nextDigit = n % 10;
+            if (nextDigit % 2 == 0) {
+                sumEvenNumbers += nextDigit;
+            }
+            n /= 10;
+        }
+        return sumEvenNumbers;
+    }
+
+    public static int sumOddNumbers(int n) {
+        int sumOddNumbers = 0;
+        while (n > 0) {
+            int nextDigit = n % 10;
+            if (nextDigit % 2 != 0) {
+                sumOddNumbers += nextDigit;
+            }
+            n /= 10;
+        }
+        return sumOddNumbers;
+    }
+
+}
