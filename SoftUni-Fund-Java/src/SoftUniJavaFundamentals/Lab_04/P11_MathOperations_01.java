3e7743a194dc52fb425345ba975f0619e78475ec
StrMilanov <str.milanov@gmail.com>
2022-10-07 18:23:49 +0300

    Problem solutions updated

--- SoftUni-Fund-Java/src/SoftUniJavaFundamentals/Lab_04/P11_MathOperations_01.java
+++ SoftUni-Fund-Java/src/SoftUniJavaFundamentals/Lab_04/P11_MathOperations_01.java
@@ -1,4 +1,45 @@
 package SoftUniJavaFundamentals.Lab_04;
 
+import java.util.Scanner;
+
 public class P11_MathOperations_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double firstNum = Double.parseDouble(scanner.nextLine());
+        String operator = scanner.nextLine();
+        double secondNum = Double.parseDouble(scanner.nextLine());
+
+        switch (operator) {
+            case "/":
+                System.out.printf("%.0f", divideNumbers(firstNum, secondNum));
+                break;
+            case "*":
+                System.out.printf("%.0f", multiplyNumbers(firstNum, secondNum));
+                break;
+            case "+":
+                System.out.printf("%.0f", sumNumbers(firstNum, secondNum));
+                break;
+            case "-":
+                System.out.printf("%.0f", subtractNumbers(firstNum, secondNum));
+                break;
+        }
+    }
+
+    public static double divideNumbers(double a, double b) {
+        return a / b;
+    }
+
+    public static double multiplyNumbers(double a, double b) {
+        return a * b;
+    }
+
+    public static double sumNumbers(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtractNumbers(double a, double b) {
+        return a - b;
+    }
+
 }
