221d36c432409325fa9b93a6a018e14d38559f3b
Stratsimir Milanov <114015950+StrMilanov@users.noreply.github.com>
2022-10-10 17:10:10 +0300

    Extracted methods

--- SoftUni-Fund-Java/src/SoftUniJavaFundamentals/Ex_04_Extra/P04_TribonacciSequence_01.java
+++ SoftUni-Fund-Java/src/SoftUniJavaFundamentals/Ex_04_Extra/P04_TribonacciSequence_01.java
@@ -8,19 +8,26 @@ public class P04_TribonacciSequence_01 {
 
         int num = Integer.parseInt(scanner.nextLine());
 
-        int[] numArray = {0, 0, 1};
         System.out.print(1 + " ");
+        printTribonacciSequence(num);
 
+    }
+
+    private static void printTribonacciSequence(int num) {
+        int[] numArray = {0, 0, 1};
         for (int i = 1; i < num; i++) {
             int sum = numArray[0] + numArray[1] + numArray[2];
             System.out.print(sum + " ");
 
-            for (int j = 0; j < 2; j++) {
-                numArray[j] = numArray[j + 1];
-            }
-            numArray[2] = sum;
+            updateArray(numArray, sum);
         }
+    }
 
+    private static void updateArray(int[] numArray, int lastNum) {
+        for (int j = 0; j < 2; j++) {
+            numArray[j] = numArray[j + 1];
+        }
+        numArray[2] = lastNum;
     }
 
 }
