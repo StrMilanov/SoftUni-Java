fc781d57f91c243d5189cd217af795185b5e7349
StrMilanov <str.milanov@gmail.com>
2022-12-04 16:10:08 +0200

    Test class added

---
+++ SoftUni-Fund-Java/src/SoftUniJavaFundamentals/test.java
@@ -0,0 +1,10 @@
+package SoftUniJavaFundamentals;
+
+public class test {
+    public static void main(String[] args) {
+
+        boolean isTrue = 100f == 100d;
+        System.out.println(isTrue);
+
+    }
+}
