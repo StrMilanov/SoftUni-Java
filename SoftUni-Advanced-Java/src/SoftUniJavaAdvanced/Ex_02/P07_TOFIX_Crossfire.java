9e6839ca346be208585f306eeacdfd4372fb058b
Stratsimir Milanov <str.milanov@gmail.com>
2023-01-23 14:38:45 +0200

    Changed names

--- SoftUni-Advanced-Java/src/SoftUniJavaAdvanced/Ex_02/P07_TOFIX_Crossfire.java
+++ SoftUni-Advanced-Java/src/SoftUniJavaAdvanced/Ex_02/P07_TOFIX_Crossfire.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class P07_Crossfire {
+public class P07_TOFIX_Crossfire {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
